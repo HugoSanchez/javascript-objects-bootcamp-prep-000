9ba05e404e7492f4976512e9b6323bf392d55e53
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-06 09:21:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = {
   NotoriousBIG : 'Juicy',
   Kase : 'Javat y Kamel',
@@ -9,3 +10,6 @@ function updatePlaylist(object, artist, songTitle){
   return playlist
 }
 
+=======
+var playlist = {'NotoriousBIG' : 'Juicy',
+>>>>>>> fff8a6f2f3e199f58e6be5fef42ec6a4c97859c9
